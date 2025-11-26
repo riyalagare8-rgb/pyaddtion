a28783bfd73667c86dea1961af2b707813a436d4
riyalagare8-rgb <riyalagare8@gmail.com>
2025-11-26 11:24:17 +0530

    Fix test function name for negative numbers

--- test_addtion.py
+++ test_addtion.py
@@ -3,7 +3,7 @@ from addtion import add
 def test_add_postitive_numbers():
     assert add(2, 3) ==5
 
-    def test_add_negative_number():
+    def test_add_negative_numbers():
         assert add(-4, -6) == -10
 
         def test_add_zero():
